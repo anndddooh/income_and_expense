fcf89b6f5ab7de0b01b8a4e592a8b22a52692fed
Hironori Ando <annndddddooooooo@gmail.com>
2020-05-10 19:51:41 +0900

    不要なパッケージを削除

--- income_and_expense/views.py
+++ income_and_expense/views.py
@@ -1,13 +1,11 @@
 import datetime
-
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from cerberus import Validator
 from dateutil.relativedelta import relativedelta
 from bootstrap_modal_forms.generic import (
     BSModalCreateView, BSModalUpdateView, BSModalDeleteView
