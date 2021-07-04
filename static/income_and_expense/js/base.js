abe9d001a13f02fc47f5dd75670fde1478ebdc02
Hironori Ando <annndddddooooooo@gmail.com>
2021-07-05 00:36:54 +0900

    年月を移動する際にボタンクリックさせるよう変更

--- static/income_and_expense/js/base.js
+++ static/income_and_expense/js/base.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     var this_mon = nav_item_date.data('mon');
     $('#current_month').val(this_year + '-' + to_double_digits(this_mon));
 
-    $('#current_month').change(function () {
+    $('#move_month').on('click', function () {
         var year_and_mon = $('#current_month').val().split('-');
         var path_name = $('#current_month').data('path-name');
         var url = '/income_and_expense/move_another_page' + '?path_name=' + path_name + '&year=' + year_and_mon[0] + '&month=' + year_and_mon[1];
         location.href = url;
-    })
+    });
 });
