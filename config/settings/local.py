8857ca9dfdd246d4b73ef075066455c53748e240
Hironori Ando <annndddddooooooo@gmail.com>
2022-07-10 02:12:56 +0900

    ローカル環境のDBをPostgreSQLに変更し、収支画面のテーブル色を変更

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'income_and_expense',
+        'USER': 'inex',
+        'PASSWORD': 'inex',
+        'HOST': '',
+        'PORT': ''
     }
 }
